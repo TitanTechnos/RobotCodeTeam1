20ef26ee0e94ffe38d427dc34b682a01c7e46c0c
AAces <djp0908@yahoo.com>
2018-01-29 16:45:08 -0700

    fix name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorAutoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorAutoRed.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous(name = "Color - Blue Team", group = "Autonomous")
+@Autonomous(name = "Color - Red Team", group = "Autonomous")
 //@Disabled
 public class ColorSensorAutoRed extends LinearOpMode {
 
