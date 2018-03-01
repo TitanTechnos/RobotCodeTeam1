9f784cefc1eb988e131baf1dbce1f902b10e3a30
AAces <djp0908@yahoo.com>
2018-03-01 07:55:14 -0700

    Compact Driving

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompactDriving.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class CompactDriving extends OpMode{
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private Hardware robot = new Hardware(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initializing");
+
+        //Initialize Robot
+        robot.init(hardwareMap);
+        robot.colorSensor.enableLed(false);
+
+        telemetry.addData("Status", "Initialized");
+    }
+
+    @Override
+    public void loop() {
+        double leftPower = 0;
+        double rightPower = 0;
+
+        if(gamepad1.right_stick_y != 0 && gamepad1.left_stick_x == 0){
+            leftPower = gamepad1.right_stick_y;
+            rightPower = gamepad1.right_stick_y;
+        } else if(gamepad1.left_stick_x != 0 && gamepad1.right_stick_y == 0) {
+            if(gamepad1.left_stick_x > 0){
+                rightPower = gamepad1.left_stick_x;
+                leftPower = -gamepad1.left_stick_x;
+            } else {
+                rightPower = -gamepad1.left_stick_x;
+                leftPower = gamepad1.left_stick_x;
+            }
+        } else if(gamepad1.left_stick_x != 0){
+            leftPower = gamepad1.right_stick_y;
+            rightPower = gamepad1.right_stick_y;
+            if(gamepad1.left_stick_x > 0){
+                double x = -Math.abs(gamepad1.left_stick_x) +1;
+                rightPower *= x;
+            } else {
+                double x = -Math.abs(gamepad1.left_stick_x) +1;
+                leftPower *= x;
+            }
+        }
+
+    }
+}
